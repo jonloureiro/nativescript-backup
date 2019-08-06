64fb85f0a131ed54119d61b9adf02da4b26ab264
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-08-05 21:55:02 -0400

    reorganizando o login e colocando o loading

--- app/views/login/login-page.ts
+++ app/views/login/login-page.ts
@@ -1,11 +1,53 @@
+import { Button } from "tns-core-modules/ui/button";
 import { Page } from 'tns-core-modules/ui/page';
-import { EventData, fromObject } from "tns-core-modules/data/observable";
-import { user, singIn, singUp } from './login-page.view-models';
+import { EventData } from "tns-core-modules/data/observable";
+import { user, login, register } from '../../view-models/user';
 
-export function navigatingTo(args: EventData) {
+
+
+const navigatingTo = (args: EventData) => {
   const page = <Page> args.object;
   page.actionBarHidden = true;
   page.bindingContext = user;
 }
 
-export { user, singIn, singUp }
+const singIn = (args: EventData) => {
+  const button = <Button> args.object;
+  const viewModal = button.showModal('views/loading/loading-modal', {
+    context: null,
+    closeCallback: null,
+  });
+
+  const email = user.get('email');
+  const password = user.get('password');
+
+  login(email, password)
+    .then(sucess => {
+      viewModal.closeModal();
+      if (sucess) {
+      } /* TODO: ELSE */
+    })
+    .catch(() => viewModal.closeModal());
+}
+
+const singUp = (args: EventData) => {
+  const button = <Button> args.object;
+  const viewModal = button.showModal('views/loading/loading-modal', {
+    context: null,
+    closeCallback: null,
+  });
+  const name = user.get('name');
+  const email = user.get('email');
+  const password = user.get('password');
+
+
+  register(name, email, password)
+    .then(sucess => {
+      viewModal.closeModal();
+      if (sucess) {
+      } /* TODO: ELSE */
+    })
+    .catch(() => viewModal.closeModal());
+}
+
+export { navigatingTo, user, singIn, singUp }
